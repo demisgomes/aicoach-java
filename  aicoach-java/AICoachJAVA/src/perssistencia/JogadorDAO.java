26401dc8e2dfd1c923492bc1f0ba1d35b1a8c05c
renanufrpe <renanufrpe@gmail.com>
2014-07-16 04:30:41 +0000

    atributo 'id' na classe 'Jogador' e métodos de inserção e deleção na classe 'JogadorDAO'.(em teste)

---  aicoach-java/AICoachJAVA/src/perssistencia/JogadorDAO.java
+++  aicoach-java/AICoachJAVA/src/perssistencia/JogadorDAO.java
@@ -1,5 +1,37 @@
 package perssistencia;
 
-public class JogadorDAO {
+import bd.Banco;
+import dominio.Jogador;
 
+public class JogadorDAO {
+	Banco banco = new Banco();
+	
+	public void JogadorDAO () {
+		
+		
+	}
+	
+	public void inserirJogador(Jogador jogador){
+		String sql = "Insert into Jogador (nome, email, data_de_nascimento, senha) VALUES('"
+				+ jogador.getNome()
+				+ "','"
+				+ jogador.getPeso()
+				+ "','"
+				+ jogador.getAltura()
+				+ "','"
+				+ jogador.getTime()
+				+ "','"
+				+ jogador.isTemCondicao()
+				+ "','"
+				+ jogador.getIdade() + "')";
+		
+		banco.executarSQL(sql);
+		
+	}
+	public void excluirJogador(Jogador jogador){
+		String sql = "DELETE FROM jogador WHERE id LIKE '"+jogador.getId()+"'";
+		banco.executarSQL(sql);
+	}
+	
+	
 }
