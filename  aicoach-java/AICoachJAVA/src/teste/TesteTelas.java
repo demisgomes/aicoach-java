5eab46cad8a80e35f77ca51e97be62aa25983a17
demismg72 <demismg72@gmail.com>
2014-07-18 16:07:17 +0000

    telas melhores

---
+++  aicoach-java/AICoachJAVA/src/teste/TesteTelas.java
@@ -0,0 +1,14 @@
+package teste;
+
+import gui.TelaInicial;
+
+public class TesteTelas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TelaInicial tela= new TelaInicial("AiCoach", 300, 300);
+		tela.setVisible(true);
+
+	}
+
+}
