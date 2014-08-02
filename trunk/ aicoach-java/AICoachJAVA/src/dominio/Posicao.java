3e46e5d0d82cad9b5cc684f66c6c7aa9adad2797
demismg72@gmail.com <demismg72@gmail.com@c181d396-0345-638d-2248-31173eaa053f>
2014-08-02 05:34:22 +0000

    Inserir jogador em andamento

--- trunk/ aicoach-java/AICoachJAVA/src/dominio/Posicao.java
+++ trunk/ aicoach-java/AICoachJAVA/src/dominio/Posicao.java
@@ -42,6 +42,94 @@ public class Posicao {
 	
 	public Posicao(String nome){
 			setNome(nome);
+			/*int posicaoTela=0;
+			if(nome.toLowerCase().equals("Goleiro")){
+				posicaoTela=1;
+			}
+			if(posicaoTela==2){
+				setNome("Lateral");
+			}
+			if(posicaoTela==3){
+				setNome("Zagueiro");
+			}
+			if(posicaoTela==4){
+				setNome("Zagueiro");
+			}
+			if(posicaoTela==5){
+				setNome("Zagueiro");
+			}
+			if(posicaoTela==6){
+				setNome("Lateral");
+			}
+			if(posicaoTela==7){
+				setNome("Lateral");
+			}
+			if(posicaoTela==8){
+				setNome("Volante");
+			}
+			if(posicaoTela==9){
+				setNome("Volante");
+			}
+			if(posicaoTela==10){
+				setNome("Volante");
+			}
+			if(posicaoTela==11){
+				setNome("Lateral");
+			}
+			if(posicaoTela==12){
+				setNome("Meia Lateral");
+			}
+			if(posicaoTela==13){
+				setNome("Meia Central");
+			}
+			if(posicaoTela==14){
+				setNome("Meia Central");
+			}
+			if(posicaoTela==15){
+				setNome("Meia Central");
+			}
+			if(posicaoTela==16){
+				setNome("Meia Lateral");
+			}
+			if(posicaoTela==17){
+				setNome("Meia Lateral");
+			}
+			if(posicaoTela==18){
+				setNome("Meia Atacante");
+			}
+			if(posicaoTela==19){
+
+				setNome("Meia Atacante");
+			}
+			if(posicaoTela==20){
+
+				setNome("Meia Atacante");
+			}
+			if(posicaoTela==21){
+
+				setNome("Meia Lateral");
+			}
+			if(posicaoTela==22){
+				setNome("Ponta");
+			}
+			if(posicaoTela==23){
+
+				setNome("Segundo Atacante");
+			}
+			if(posicaoTela==24){
+
+				setNome("Centro Avante");
+			}
+			if(posicaoTela==25){
+
+				setNome("Segundo Atacante");
+			}
+			if(posicaoTela==26){
+
+				setNome("Ponta");
+			}
+
+*/			
 	}
 	
 	public Posicao(int posicaoTela){
