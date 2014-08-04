0b52d7f8b938aa0337e1013138d5838e80c1706a
c.spinelli93@gmail.com <c.spinelli93@gmail.com@c181d396-0345-638d-2248-31173eaa053f>
2014-08-04 20:23:16 +0000

    controlador Tatica

--- trunk/ aicoach-java/AICoachJAVA/src/controle/ControladorTatica.java
+++ trunk/ aicoach-java/AICoachJAVA/src/controle/ControladorTatica.java
@@ -1,19 +1,155 @@
 package controle;
 
+import gui.TelaCriarTatica;
+import gui.TelaInserirAlterarEsquema;
+import gui.TelaInserirCombaterTatica;
+import gui.TelaListaTimes;
 import gui.TelaTatica;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+import perssistencia.JogadorDAO;
+import perssistencia.TaticaDAO;
+import perssistencia.TimeDAO;
+
+import negocio.AlgoritmoTatica;
 
 import dominio.ButtonAiCoach;
 import dominio.Posicao;
+import dominio.Tatica;
 import dominio.Time;
 
 public class ControladorTatica {
 	
 	
-	public void acaoBotoes(){
-		//todo
+	public void acaoBotoes(final TelaTatica tela){
+		TelaTatica.getBtnAlterarEsquema().addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				TelaInserirAlterarEsquema telaInserir=new TelaInserirAlterarEsquema(TelaTatica.getTime());
+				telaInserir.setVisible(true);
+				
+			}
+		});
+		
+		TelaTatica.getBtnSugerirTtica().addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Time time = TelaTatica.getTime();
+				for (int j = 0; j < time.getJogadores().size(); j++) {
+					time.getJogadores().get(j).setEscolhido(0);
+					
+				}
+				
+				AlgoritmoTatica aTatica=new AlgoritmoTatica();
+				aTatica.sugerirTatica(time, TelaTatica.getListaTaticas());
+				mostrarBotoes();
+				TelaTatica.naoSalvar=false;
+			}
+		});
+		
+		TelaTatica.getBtnEscolherMelhoresJogadores().addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Time time = TelaTatica.getTime();
+				for (int j = 0; j < time.getJogadores().size(); j++) {
+					time.getJogadores().get(j).setEscolhido(0);
+					
+				}
+
+				TaticaDAO taticaDAO=new TaticaDAO();
+				Tatica taticaEscolhida=taticaDAO.retornarTatica(TelaTatica.getComboBox().getSelectedItem().toString());
+				
+				
+				
+				time.setTatica(taticaEscolhida);
+				AlgoritmoTatica aTatica= new AlgoritmoTatica();
+				aTatica.SugerirJogadores(time);
+				mostrarBotoes();
+				TelaTatica.naoSalvar=false;
+			}
+			
+		});
+		
+		TelaTatica.getBtnCombaterTtica().addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				TelaInserirCombaterTatica telaCombaterTatica=new TelaInserirCombaterTatica(TelaTatica.getTime());
+				telaCombaterTatica.setVisible(true);
+				tela.dispose();
+			}
+		});
+		
+		TelaTatica.getBtnCriarNovaTtica().addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				TelaCriarTatica telaCriarTatica= new TelaCriarTatica();
+				telaCriarTatica.setVisible(true);
+				tela.dispose();
+			}
+		});
+		
+		TelaTatica.getBtnSalvarTtica().addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ArrayList<ButtonAiCoach> listaBotoes =TelaTatica.getListaBotoes();
+				ArrayList<Integer> listaJogador = TelaTatica.getListaJogador();
+				Time time = TelaTatica.getTime();
+				
+				if(TelaTatica.naoSalvar){
+					JOptionPane.showMessageDialog(null, "Por enquanto, o Combater Tática não pode ser salvo.");
+				}
+				
+				else{
+					
+					JogadorDAO jdao=new JogadorDAO();
+					jdao.tirarIdPosicaoJogador(time);
+					for(int i=0;i<listaBotoes.size();i++){
+						if(listaBotoes.get(i).getJogador()!=null){
+							jdao.inserirIdPosicaoJogador(listaBotoes.get(i).getJogador(), listaBotoes.get(i).getId());
+							//listaJogador.add(listaBotoes.get(i).getJogador().getId());
+						}
+					}
+					TimeDAO timeDAO=new TimeDAO();
+					timeDAO.inserirTimeTatica(time, listaJogador);
+					JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+				}
+			}
+		});
+		
+		TelaTatica.getBtnSubstituir().addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Time time = TelaTatica.getTime();
+				TaticaDAO taDAO=new TaticaDAO();
+				boolean substituir = TelaTatica.isSubstituir();
+				
+				AlgoritmoTatica tatica= new AlgoritmoTatica();
+				tatica.alterarEsquema(time, 1, taDAO.retornarTatica(time.getTatica().getNome()));
+				substituir=true;
+				mostrarBotoes();
+				System.out.println("depois da confusão");
+				System.out.println("-----------------------");
+				for (Posicao p : time.getTatica().getPosicoes()) {
+					System.out.println(p.getNome()+" será "+ p.getJogador().getNome()+ " "+ p.getJogador().getPosicaoAtual().getPontuacao()+" "+p.getIdPosicaoTela());
+				}
+				TelaTatica.naoSalvar=false;
+			}
+		});
+		
+		TelaTatica.getBtnVoltar().addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				TelaListaTimes telaTimes = new TelaListaTimes();
+				telaTimes.setVisible(true);
+				tela.dispose();
+			}
+		});
 	}
 	
 
